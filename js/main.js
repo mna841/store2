53efab00adde38961365ed3920efdab282f80486
mina <mna03549@gmail.com>
2024-09-02 22:45:05 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function(){
     $('.togggle_btn').on('click', function () {
-        if ($('#header').hasClass('openn')) {
+        if ($('#header').hasClass('open')) {
             $('#header').removeClass('open');
         } else {
             $('#header').addClass('open');
